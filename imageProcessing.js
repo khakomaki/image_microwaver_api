41ea4972b5721cb72f1f1b83a4e37b14c0693ebd
khakomaki <hakomaki.karri@gmail.com>
2024-01-11 22:50:13 +0200

    added reduceFileSize

--- imageProcessing.js
+++ imageProcessing.js
@@ -1,6 +1,6 @@
 const sharp = require('sharp');
 
-const strech = async (image, intensity, min=-100, max=100) => {
+const strech = async (image, intensity, min=-99, max=100) => {
     // throws error if the intensity isn't in the range
     if (intensity < min || max < intensity) {
         throw new Error(`Given intensity '${intensity}' wasn't in the range [${min} - ${max}]`);
@@ -12,12 +12,27 @@ const strech = async (image, intensity, min=-100, max=100) => {
     const newWidthScale = 1 + (intensity / 100);
     const newWidth = Math.round(metadata.width * newWidthScale);
     return sharp(image)
-        .resize({ width: newWidth })
+        .resize({ width: newWidth, height: metadata.height, fit: 'fill' })
+        .toBuffer();
+};
+
+const reduceFileSize = async (image, intensity, min=1, max=100) => {
+    // throws error if the intensity isn't in the range
+    if (intensity < min || max < intensity) {
+        throw new Error(`Given intensity '${intensity}' wasn't in the range [${min} - ${max}]`);
+    }
+
+    // changing intensity -> jpeg quality (flipping min & max)
+    const quality = max - (intensity - min);
+
+    return sharp(image)
+        .jpeg({ quality: quality })
         .toBuffer();
 };
 
 const functions = {
     Normal: strech,
+    Grill: reduceFileSize
 };
 
 const processImage = async (image, mode, intensity) => {
