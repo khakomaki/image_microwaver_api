5edc4042add59006d5f36ee9271b1b4290062e6d
khakomaki <hakomaki.karri@gmail.com>
2024-01-12 15:35:49 +0200

    added saturation function

--- imageProcessing.js
+++ imageProcessing.js
@@ -16,6 +16,19 @@ const strech = async (image, intensity, min=-99, max=100) => {
         .toBuffer();
 };
 
+const saturate = async (image, intensity, min=-100, max=100) => {
+    // throws error if the intensity isn't in the range
+    if (intensity < min || max < intensity) {
+        throw new Error(`Given intensity '${intensity}' wasn't in the range [${min} - ${max}]`);
+    }
+
+    // multiplier for saturation
+    const saturationMultiplier = 1 + (intensity / 100);
+    return sharp(image)
+        .modulate({ saturation: saturationMultiplier })
+        .toBuffer();
+}
+
 const reduceFileSize = async (image, intensity, min=1, max=100) => {
     // throws error if the intensity isn't in the range
     if (intensity < min || max < intensity) {
@@ -32,6 +45,7 @@ const reduceFileSize = async (image, intensity, min=1, max=100) => {
 
 const functions = {
     Normal: strech,
+    Defrosting: saturate,
     Grill: reduceFileSize
 };
 
