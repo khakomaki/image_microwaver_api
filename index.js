72a203eb4b7cf2cdb3c201b4675689415a43db39
khakomaki <hakomaki.karri@gmail.com>
2024-01-26 22:15:31 +0200

    prevented too large image files from being uploaded

--- index.js
+++ index.js
@@ -3,37 +3,54 @@ const multer = require('multer');
 const express = require('express');
 const bodyParser = require('body-parser');
 const imageProcessor = require('./imageProcessing')
+const app = express();
 
 const port = 3001;
-const app = express();
+const maxFileSize = 5 * 1024 * 1024; // 5MB
+
 const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+const upload = multer({ 
+    storage: storage, 
+    limits: { fileSize: maxFileSize } 
+}).single('image');
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.post('/process-image', upload.single('image'), async (req, res) => {
-    const { image, mode, intensity } = req.body;
-    const imageBuffer = req.file.buffer;
+app.post('/process-image', (req, res) => {
+    upload(req, res, async (err) => {
+        // handles errors in image upload
+        if (err) {
+            if (err.code === 'LIMIT_FILE_SIZE') {
+                console.log(`Uploaded image exceeded maximum file size`);
+                return res.status(400).json({ error: `Image exceeds the maximum file size` });
+            } else {
+                console.log(`Unexpected error in image upload: ${err}`);
+                return res.status(400).json({ error: `Internal server error` });
+            }
+        }
+        
+        const { mode, intensity } = req.body;
+        const imageBuffer = req.file.buffer;
 
-    // checks that intensity is a number
-    intensityFloat = parseFloat(intensity);
+        // checks that intensity is a number
+        intensityFloat = parseFloat(intensity);
+        if (isNaN(intensityFloat)) {
+            return res.status(400).json({ error: `Given intensity '${intensity}' wasn't a number` });
+        }
 
-    if (isNaN(intensityFloat)) {
-        return res.status(400).json({ error: `Given intensity '${intensity}' wasn't a number` });
-    }
-
-    try {
         // processes image
-        const processedImage = await imageProcessor(imageBuffer, mode, intensityFloat);
+        try {
+            const processedImage = await imageProcessor(imageBuffer, mode, intensityFloat);
 
-        // sends the processed image back
-        res.set('Content-Type', 'image/jpeg');
-        res.send(processedImage);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-        console.log(error.message);
-    }
+            // sends the processed image back
+            res.set('Content-Type', 'image/jpeg');
+            res.send(processedImage);
+        } catch (error) {
+            console.error(`Error in processing image: ${error.message}`);
+            return res.status(400).json({ error: error.message });
+        }
+    });
 });
 
 app.get('/', (req, res) => {
