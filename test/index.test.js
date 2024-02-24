7d6b592144d9a4af586528e11bc3bc9757ac1d7b
khakomaki <hakomaki.karri@gmail.com>
2024-02-24 22:20:04 +0200

    added tests for uploaded image

--- test/index.test.js
+++ test/index.test.js
@@ -4,6 +4,12 @@ const { app, close } = require('../index');
 
 // test image paths
 const basic_image_path = path.join(__dirname, 'testImages', 'basic_image.jpg');
+const unsupported_type_image_path_pix = path.join(__dirname, 'testImages', 'unsupported_file_type.pix');
+const unsupported_type_image_path_jpg_pix = path.join(__dirname, 'testImages', 'unsupported_file_type.jpg.pix');
+const multiple_types_image = path.join(__dirname, 'testImages', 'many.periods.and.file.types.png.gif.jpg');
+const no_type_image = path.join(__dirname, 'testImages', 'file');
+const no_name_image = path.join(__dirname, 'testImages', '.jpg');
+// const too_large_image_path = path.join(__dirname, 'testImage', 'too_large_image_size.JPG');
 
 describe('GET /', () => {
     it("should return welcome message", async () => {
@@ -15,7 +21,9 @@ describe('GET /', () => {
 });
 
 describe('POST /process-image', () => {
-    /* ========== parameters ========== */
+    
+    /* ==================== parameters ==================== */
+
     it("should process image", async () => {
         const response = await request(app)
             .post('/process-image')
@@ -52,7 +60,7 @@ describe('POST /process-image', () => {
         const response = await request(app)
             .post('/process-image')
             .attach('image', basic_image_path)
-            .field('mode', 'invalid-mode')
+            .field('mode', 'Normal')
             // missing intensity field
         
         expect(response.status).toBe(400);
@@ -74,14 +82,87 @@ describe('POST /process-image', () => {
         const response = await request(app)
             .post('/process-image')
             // missing image attachment
-            .field('mode', 'invalid-mode')
+            .field('mode', 'Normal')
             .field('intensity', 50);
         
         expect(response.status).toBe(400);
         expect(response.body.error).toBe("Image wasn't provided");
     });
 
-    /* ========== image ========== */
+    it("should throw error if parameters are missing", async () => {
+        const response = await request(app)
+            .post('/process-image')
+            // missing all parameters
+        
+        expect(response.status).toBe(400);
+    });
+
+    /* ==================== image ==================== */
+
+    // TODO: test for uploading too large image
+
+    it("should throw error if image file type isn't supported", async () => {
+        const response = await request(app)
+            .post('/process-image')
+            .attach('image', unsupported_type_image_path_pix)
+            .field('mode', 'Normal')
+            .field('intensity', 50);
+        
+        expect(response.status).toBe(400);
+        expect(response.body.error).toBe("Uploaded file type wasn't supported");
+    });
+
+    it("should throw error if image file type isn't supported", async () => {
+        const response = await request(app)
+            .post('/process-image')
+            .attach('image', unsupported_type_image_path_jpg_pix) // including jpg in file name shouldn't affect outcome
+            .field('mode', 'Normal')
+            .field('intensity', 50);
+        
+        expect(response.status).toBe(400);
+        expect(response.body.error).toBe("Uploaded file type wasn't supported");
+    });
+
+    it("should throw error if the uploaded file isn't image", async () => {
+        const response = await request(app)
+            .post('/process-image')
+            .attach('image', no_type_image)
+            .field('mode', 'Normal')
+            .field('intensity', 50);
+        
+        expect(response.status).toBe(400);
+        expect(response.body.error).toBe("Uploaded file type wasn't supported");
+    });
+
+    it("should process image without name", async () => {
+        const response = await request(app)
+            .post('/process-image')
+            .attach('image', no_name_image)
+            .field('mode', 'Normal')
+            .field('intensity', 50);
+        
+        expect(response.status).toBe(200);
+    });
+
+    it("should process image correctly with multiple file types in name", async () => {
+        const response = await request(app)
+            .post('/process-image')
+            .attach('image', multiple_types_image)
+            .field('mode', 'Normal')
+            .field('intensity', 50);
+        
+        expect(response.status).toBe(200);
+    });
+
+    it("should process image without name", async () => {
+        const response = await request(app)
+            .post('/process-image')
+            .attach('image', no_name_image)
+            .field('mode', 'Normal')
+            .field('intensity', 50);
+        
+        expect(response.status).toBe(200);
+    });
 });
 
 // closes the server after tests
